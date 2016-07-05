d95a09e2b119000227e6b6239492ed9370744e44
lca slax <luca.slavazza@atscom.it>
2016-07-05 16:20:00 +0200

    ricerca immagini

--- execute.php
+++ execute.php
@@ -1,25 +1,22 @@
 <?php
+// insert here your Bot API token
+define("BOT_TOKEN", "...");
 $content = file_get_contents("php://input");
 $update = json_decode($content, true);
-
 if(!$update)
 {
   exit;
 }
-
 $message = isset($update['message']) ? $update['message'] : "";
-$messageId = isset($message['message_id']) ? $message['message_id'] : "";
 $chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
-$firstname = isset($message['chat']['first_name']) ? $message['chat']['first_name'] : "";
-$lastname = isset($message['chat']['last_name']) ? $message['chat']['last_name'] : "";
-$username = isset($message['chat']['username']) ? $message['chat']['username'] : "";
-$date = isset($message['date']) ? $message['date'] : "";
 $text = isset($message['text']) ? $message['text'] : "";
-
-$text = trim($text);
-$text = strtolower($text);
-
-header("Content-Type: application/json");
-$parameters = array('chat_id' => $chatId, "text" => $text);
-$parameters["method"] = "sendMessage";
-echo json_encode($parameters);
+$botUrl = "https://api.telegram.org/bot" . BOT_TOKEN . "/sendPhoto";
+// change image name and path
+$postFields = array('chat_id' => $chatId, 'photo' => new CURLFile(realpath("image.png")), 'caption' => $text);
+$ch = curl_init(); 
+curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type:multipart/form-data"));
+curl_setopt($ch, CURLOPT_URL, $botUrl); 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+curl_setopt($ch, CURLOPT_POSTFIELDS, $postFields);
+// read curl response
+$output = curl_exec($ch);
