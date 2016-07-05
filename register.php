be4636eb4507350c44b73482cc4fb36cce35cc3d
lca slax <luca.slavazza@atscom.it>
2016-07-05 15:51:13 +0200

    registrata applicazione margelet2bot

--- register.php
+++ register.php
@@ -1,8 +1,8 @@
 <?php
 
 // PARAMETRI DA MODIFICARE
-$WEBHOOK_URL = 'https://{APP NAME}.herokuapp.com/execute.php';
-$BOT_TOKEN = '{TOKEN}';
+$WEBHOOK_URL = 'https://margelet2bot.herokuapp.com/execute.php';
+$BOT_TOKEN = '251574507:AAGGoDWsen2NKJdJMdoa_4WKWf-xGvjkWNQ';
 
 // NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
 $API_URL = 'https://api.telegram.org/bot' . $BOT_TOKEN .'/';
